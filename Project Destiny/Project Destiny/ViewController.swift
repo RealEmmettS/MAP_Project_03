9c6b51fc52418d7b15a2e2f1e040841b1fc59c7e
Emmett Shaughnessy <github@emmetts.dev>
2021-08-27 10:37:08 -0500

    init

---
+++ Project Destiny/Project Destiny/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project Destiny
+//
+//  Created by Emmett Shaughnessy on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBOutlet weak var roomLabel: UILabel!
+    @IBOutlet weak var story: UITextView!
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    
+    @IBAction func choice1(_ sender: Any) {
+    }
+    
+    @IBAction func choice2(_ sender: Any) {
+    }
+    
+    
+    
+
+
+}
+
